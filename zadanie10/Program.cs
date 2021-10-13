dd9d934455cb73b82be9ca12569fd419f48c10d4
Alex <Alex@LAPTOP-GV3B110K>
2021-10-13 17:03:08 +0200

    Добавьте файлы проекта.

---
+++ zadanie10/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uravnenie uravn = new Uravnenie(25.5, 31.7);
+            double x = uravn.Root();
+            Console.WriteLine("Корень уравнения 0={0}x+{1} равен {2}", uravn.k, uravn.b, x);
+            Console.ReadKey();
+        }
+
+
+        //структура уравнения
+        public struct Uravnenie
+        {
+            public double k;
+            public double b;
+
+             //конструктор инициализации переменных
+            public Uravnenie (double k, double b)
+            {
+                this.k = k;
+                this.b = b;
+            }
+
+            //метод расчета корня уравнения
+            public double Root()
+            {
+                double x = b / k;
+                return x;
+            }
+
+        }
+    }
+}
